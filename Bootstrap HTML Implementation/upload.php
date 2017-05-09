e960e561194546c70ae99346f465195ba38167d0
Milo0192 <myles.wardell212@gmail.com>
2017-05-09 15:50:00 +1000

    Working file upload
    Added php test file

--- Bootstrap HTML Implementation/upload.php
+++ Bootstrap HTML Implementation/upload.php
@@ -1,3 +1,17 @@
 <?php
-echo 'hello.';
+$uploaded = array();
+
+if(!empty($_FILES['file']['name'][0])) {
+    foreach($_FILES['file']['name'] as $position => $name) {
+        if (move_uploaded_file($_FILES['file']['tmp_name'][$position], 'uploads/' . $name)) {
+            $uploaded[] = array(
+                'name' => $name,
+                'file' => 'uploads/' . $name
+            );
+        }
+    }
+}
+
+echo json_encode($uploaded);
+
 ?>
